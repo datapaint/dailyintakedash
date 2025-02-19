6e5b57f3fe976ba9ede0a3c48be8afaa869c44b3
Aakash Desai <aakashdesai@Aakashs-Mac-mini.local>
2025-02-19 09:40:44 -0800

    update env var dash pass

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import dash
+import os, json
 import dash_auth
 from dash import Dash, dcc, html, Input, Output, callback
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
 
+# Get the environment VALID_USERNAME_PASSWORD_PAIRS
+auth_pairs_json = os.environ.get("VALID_USERNAME_PASSWORD_PAIRS")
+
+if auth_pairs_json:
+    try:
+        valid_username_password_pairs = json.loads(auth_pairs_json)
+    except json.JSONDecodeError:
+        print("Error: Invalid JSON format for VALID_USERNAME_PASSWORD_PAIRS")
+        valid_username_password_pairs = {}
+else:
+    print("Warning: VALID_USERNAME_PASSWORD_PAIRS not set")
+    valid_username_password_pairs = {}
+
 # Add authentication
 auth = dash_auth.BasicAuth(
     app,
-    VALID_USERNAME_PASSWORD_PAIRS
+    valid_username_password_pairs
 )
 
 # Define the layout of the dashboard
